0a83c49289f8f7d7bd01e4cb3633480fada0eca0
MogutovEvgenyi <fantast08@mail.ru>
2023-08-18 00:25:22 +0500

    решение задачи 15

---
+++ Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+if (dayWeek == 6 || dayWeek == 7)
+{
+    Console.WriteLine("{0} -> да", dayWeek);
+}
+else if (dayWeek >= 1 && dayWeek <= 5)
+{
+    Console.WriteLine("{0} -> нет ", dayWeek);
+}
+else
+{
+    Console.WriteLine("{0} -> дня недели с такой цифрой нет", dayWeek);
+}
